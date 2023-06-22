f6e61a0a4b79314ea5ac6637f7fa3b35c2a55155
alirao2880 <alirao2880@gmail.com>
2023-06-22 15:41:23 +0100

    Changed Formatting Of Code To Make Consistent With Other Files

--- src/components/final/index.js
+++ src/components/final/index.js
@@ -12,12 +12,12 @@ export default function FinalScreen({ score }) {
     return newGame ?
       <Topics /> :
       (
-      <div className="flex h-screen">
-          <div className="m-auto xl:w-[65%] lg:w-[75%] md:w-[87.5%] sm:w-[92.5%] text-center flex flex-col items-center justify-center">
-              <h5 className="text-3xl font-semibold tracking-wide text-gray-900 my-10">Your Final Score Is: {score}</h5>
-              <button className="text-white focus:outline-none ring-4 font-medium rounded-full text-lg px-7 py-3 text-center mr-2 mb-2 bg-green-600 hover:bg-green-700 ring-green-800" onClick={handleNewGame}>Begin New Quiz</button>
+          <div className="flex h-screen">
+              <div className="m-auto xl:w-[65%] lg:w-[75%] md:w-[87.5%] sm:w-[92.5%] text-center flex flex-col items-center justify-center">
+                  <h5 className="text-3xl font-semibold tracking-wide text-gray-900 my-10">Your Final Score Is: {score}</h5>
+                  <button className="text-white focus:outline-none ring-4 font-medium rounded-full text-lg px-7 py-3 text-center mr-2 mb-2 bg-green-600 hover:bg-green-700 ring-green-800" onClick={handleNewGame}>Begin New Quiz</button>
+              </div>
           </div>
-      </div>
-    );
+      );
 
 }
