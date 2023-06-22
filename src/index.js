8ef672ec2339e7ba57908bd751400f0a6eb0b636
alirao2880 <alirao2880@gmail.com>
2023-06-22 15:19:21 +0100

    Main JS File Containing The App

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Topics from './components/topics';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('MCQ')).render(
+  <React.StrictMode>
+    <Topics />
+  </React.StrictMode>
+);
